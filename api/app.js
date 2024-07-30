46fb57818b8302001a42f6de5e59c576fac457b8
Mohammed Mubarak <modymu9@gmail.com>
2024-07-30 10:17:02 +0200

    chore: added a test route to check api status

--- api/app.js
+++ api/app.js
@@ -10,6 +10,11 @@ app.use(cors());
 // application routes
 app.use('/api/v1/auth', authRouter);
 
+// test route to check if the server is running
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API', statusCode: 200 });
+});
+
 // 404 error handler
 app.use((req, res) => {
   res.status(404).json({ message: 'This endpoint has not been impelemented yet', statusCode: 404 });
